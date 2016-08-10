b198009940294bd84d1846a140bee2aa7a1b5514
Stuntman Mike <stuntman.mike@mail.xyz>
2016-08-10 16:00:55 +0200

    added mvMAQLClient-no-gui

---
+++ mvMAQLClient-no-gui/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mv.MAQL.Helper;
+using mv.MAQL.Data.Handling;
+
+namespace mvMAQLClient_no_gui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
