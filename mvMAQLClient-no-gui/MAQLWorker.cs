35e40f2f00444043933140a66aad07d7879c5d06
Stuntman Mike <stuntman.mike@mail.xyz>
2016-08-12 15:58:18 +0200

    reorganizing mvMAQLClient-no-gui

---
+++ mvMAQLClient-no-gui/MAQLWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace mv.MAQL
+{
+    public class MAQLWorker
+    {
+        private ConfigHandling cfg_ = new ConfigHandling();
+        private mvSQLDataHandlingBase sqldata_;
+        private Logger logger_;
+
+        public MAQLWorker( TextWriterTraceListener logger)
+        {
+            logger_ = logger as Logger;
+            cfg_ = new ConfigHandling();
+            sqldata_ = new mvOSQLDataHandling(cfg_.DatabaseName(), cfg_.ConnectionString());
+        }
+
+        public void DummyFunction()
+        {
+            sqldata_.FindData("160301893DE", "Serialnumber");
+        }
+    }
+}
--- mvMAQLClient-no-gui/Program.cs
+++ mvMAQLClient-no-gui/Program.cs
@@ -7,23 +7,18 @@ using System.Diagnostics;
 
 namespace mv.MAQL
 {
-    partial class Program
+    class Program
     {
         private static Logger logger_;
-        private static ConfigHandling cfg_;
-        private static mvSQLDataHandlingBase sqldata_;
 
         static int Main(string[] args)
         {
             Console.WriteLine(string.Format("mvMAQLClient-no-gui"));
+
             logger_ = new Logger("mvMAQLClient.log", "mvMAQLClient");
             Trace.Listeners.Add(logger_);
-
-            cfg_ = new ConfigHandling();
-            sqldata_ = new mvOSQLDataHandling(cfg_.DatabaseName(), cfg_.ConnectionString());
-
-            sqldata_.FindData("MS000241", "Serialnumber");
-            DummyFunction();
+            MAQLWorker mql = new MAQLWorker(logger_);
+            mql.DummyFunction();
 
             Console.WriteLine("Press ENTER ...");
             Console.ReadLine();
