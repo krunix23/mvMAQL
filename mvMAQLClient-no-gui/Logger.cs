56838a9fceef8906a193307fe493b696961ccc24
Stuntman Mike <stuntman.mike@mail.xyz>
2016-08-15 16:42:26 +0200

    cosmetics

--- mvMAQLClient-no-gui/Logger.cs
+++ mvMAQLClient-no-gui/Logger.cs
@@ -14,29 +14,12 @@ namespace mv.MAQL
 
         public override void WriteLine(string message)
         {
-            message += "\n";
             Console.WriteLine(message);
             Trace.Flush();
         }
 
         public override void WriteLine(string message, string category)
         {
-            //switch (category.ToLower())
-            //{
-            //    case "info":
-            //        output_.SelectionColor = Color.DarkGreen;
-            //        break;
-            //    case "warning":
-            //        output_.SelectionColor = Color.Orange;
-            //        break;
-            //    case "error":
-            //        output_.SelectionColor = Color.Red;
-            //        break;
-            //    default:
-            //        output_.SelectionColor = Color.Black;
-            //        break;
-            //}
-            message += "\n";
             Console.WriteLine(message);
             Trace.Flush();
         }
