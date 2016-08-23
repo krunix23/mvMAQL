293da182363c8cc420b2b1106009d1cf6068df52
Stuntman Mike <stuntman.mike@mail.xyz>
2016-08-23 17:17:50 +0200

    new calling  param: --hide => hide console window

--- mvMAQLClient-no-gui/Program.cs
+++ mvMAQLClient-no-gui/Program.cs
@@ -4,37 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace mv.MAQL
 {
-    class Program
+class Program
+{
+    [DllImport("Kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+    [DllImport("User32.dll")]
+    private static extern bool ShowWindow(IntPtr hWind, int cmdShow);
+
+    private static Logger logger_;
+
+    static int Main(string[] args)
     {
-        private static Logger logger_;
+        Console.WriteLine(string.Format("mvMAQLClient-no-gui"));
+        Int32 result = -1;
+        bool hideWindow = false;
 
-        static int Main(string[] args)
+        if(args.Length == 0)
         {
-            Console.WriteLine(string.Format("mvMAQLClient-no-gui"));
-            Int32 result = -1;
+            Console.WriteLine("This program must be called with params.");
+            return -1;
+        }
 
-            if(args.Length == 0)
+        if (args.Length > 0)
+        {
+            foreach (string param in args)
             {
-                Console.WriteLine("This program must be called with params.");
-                return -1;
+                if(param.Equals("--hide") == true)
+                {
+                    hideWindow = true;
+                }
             }
+        }
 
-            logger_ = new Logger("mvMAQLClient.log", "mvMAQLClient");
-            Trace.Listeners.Add(logger_);
-            MAQLWorker maql = new MAQLWorker(logger_);
-            
-            result = maql.Init(args);
+        if(hideWindow == true)
+        {
+            IntPtr hWind = GetConsoleWindow();
+            if (hWind != IntPtr.Zero)
+            {
+                ShowWindow(hWind, 0);
+            }
+        }
 
-            if (result == 0)
-                result = maql.FetchLicense();
-            
+        logger_ = new Logger("mvMAQLClient.log", "mvMAQLClient");
+        Trace.Listeners.Add(logger_);
+        MAQLWorker maql = new MAQLWorker(logger_);
+
+        result = maql.Init(args);
+
+        if (result == 0)
+            result = maql.FetchLicense();
+
+        if(hideWindow == false)
+        {
             Console.WriteLine("Press ENTER ...");
             Console.ReadLine();
-
-            return result;
         }
+
+        return result;
     }
+}
 }
