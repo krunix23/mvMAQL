46cda5b8144c6d0b1440b159e26b8382a33389af
Stuntman Mike <stuntman.mike@mail.xyz>
2016-08-26 10:41:45 +0200

    add mvMAQLUnitTest project
    SCM-2150

---
+++ mvMAQLUnitTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mv.MAQL;
+
+namespace mvMAQLUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_ConfigHandingConstructor()
+        {
+            try
+            {
+                ConfigHandling cfg_ = new ConfigHandling();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(string.Format("An exception has been thrown: {0}", ex.Message));
+            }
+        }
+
+        [TestMethod]
+        public void test_ConfigHandlingPublicMethods()
+        {
+                ConfigHandling cfg_ = new ConfigHandling();
+                Assert.IsFalse(string.IsNullOrEmpty(cfg_.ConnectionString()));
+                Assert.IsFalse(string.IsNullOrEmpty(cfg_.DatabaseName()));
+                Assert.IsFalse(string.IsNullOrEmpty(cfg_.DatabaseProvider()));
+        }
+    }
+}
